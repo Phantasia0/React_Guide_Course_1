0625a1664b056e461661fe64ecd5c36a4fef718b
서원준 <seowonjun@seowonjun-ui-MacBookPro.local>
2023-10-11 22:47:40 +0900

    section13_React.memo_useCallback_useMemo

---
+++ section13/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useCallback } from "react";
+
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevState) => !prevState);
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
