96c01349996e01ef94bd41738a701b17702453ec
서원준 <ncprog1@gmail.com>
2023-10-15 14:21:01 +0900

    section21_commit some that was not committed

--- section21/src/App.js
+++ section21/src/App.js
@@ -1,12 +1,35 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-import Products from "./pages/Products";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+// const routerDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>,
+// );
+
+// const router = createBrowserRouter(routerDefinitions);
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
   {
-    path: "/products",
-    element: <Products />,
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "products/:productId",
+        element: <ProductDetailPage />,
+      },
+    ],
   },
 ]);
 
