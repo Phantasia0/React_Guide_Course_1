620b127f8cd5ea31de30795dfbebf8091f530fb0
서원준 <ncprog1@gmail.com>
2023-10-19 03:27:45 +0900

    section29.5_ts_context_react

--- section29.5/src/store/todos-context.tsx
+++ section29.5/src/store/todos-context.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ReactElement, useState } from "react";
 import Todo from "../models/todo";
 
 type TodosContextObj = {
@@ -12,34 +12,35 @@ export const TodosContext = React.createContext<TodosContextObj>({
   addTodo: () => {},
   removeTodo: (id: string) => {},
 });
+const TodosContextProvider: React.FC<{ children: ReactElement[] }> = (
+  props,
+) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-// const TodosContextProvider: React.FC = (props) => {
-//   const [todos, setTodos] = useState<Todo[]>([]);
-//
-//   const addTodoHandler = (todoText: string) => {
-//     const newTodo = new Todo(todoText);
-//
-//     setTodos((prevTodos) => {
-//       return prevTodos.concat(newTodo);
-//     });
-//   };
-//
-//   const removeTodoHandler = (todoId: string) => {
-//     setTodos((prevTodos) => {
-//       return prevTodos.filter((todo) => todo.id !== todoId);
-//     });
-//   };
-//
-//   const contextValue: TodosContextObj = {
-//     items: todos,
-//     addTodo: addTodoHandler,
-//     removeTodo: removeTodoHandler,
-//   };
-//
-//   return (
-//     <TodosContext.Provider value={contextValue}>
-//       {props.children}
-//     </TodosContext.Provider>
-//   );
-// };
-// export default TodosContextProvider;
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {props.children}
+    </TodosContext.Provider>
+  );
+};
+export default TodosContextProvider;
