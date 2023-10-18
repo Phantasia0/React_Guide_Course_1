9103ad9a848f93a2bf116195d8ed306e12c13ccf
서원준 <ncprog1@gmail.com>
2023-10-19 03:11:11 +0900

    section29.5_simple_typescript_with_react

--- section29.5/src/App.tsx
+++ section29.5/src/App.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
 import React from "react";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  return <div></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
+    </div>
+  );
 }
 
 export default App;
